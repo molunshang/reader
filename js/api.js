8f73fc74252c20f17c4ae47bf5a3c88c1089a337
郑凯 <kai.zheng@fengjr.com>
2020-11-23 18:53:20 +0800

    icon set

--- js/api.js
+++ js/api.js
@@ -23,7 +23,8 @@ define(["mui", "jquery", "siteconfig", "common", "gbk", "linqjs"], function(mui,
 				reject();
 			}
 		};
-		mui.extend(info, option, true)
+		mui.extend(info, option, true);
+		plus.navigator.removeAllCookie();
 		mui.ajax(url, info);
 	}
 	var api = {
@@ -49,8 +50,8 @@ define(["mui", "jquery", "siteconfig", "common", "gbk", "linqjs"], function(mui,
 			})
 		}
 	};
-	api.deleteAllCookies =
-		'<script type="text/javascript">var cookies = document.cookie.split(";");for (var i = 0; i < cookies.length; i++) {var cookie = cookies[i];var eqPos = cookie.indexOf("=");var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";}</scprit>';
+	// api.deleteAllCookies =
+	// 	'<script type="text/javascript">var cookies = document.cookie.split(";");for (var i = 0; i < cookies.length; i++) {var cookie = cookies[i];var eqPos = cookie.indexOf("=");var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";}</scprit>';
 	api.parseDetail = function(doc, site) {
 		var url = doc.url;
 		var book = {
